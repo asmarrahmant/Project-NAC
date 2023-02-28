72482cba3b76b1121648881bcf7beed0687fa33e
twyllo <asmarrahmant@gmail.com>
2023-02-28 23:02:28 +0530

    html files path re-updated

--- my_project/User/urls.py
+++ my_project/User/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index',views.index, name='index'),
+    path('welcome',views.welcome, name='welcome'),
     path('home',views.home, name='home'),
     path('login',views.login, name='login'),
     path('otp',views.otp,name='otp'),
