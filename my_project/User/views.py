a7679d8236eb90a749d442242ad83dada6be8195
twyllo <asmarrahmant@gmail.com>
2023-03-01 00:36:27 +0530

    fully changed

--- my_project/User/views.py
+++ my_project/User/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def welcome(request):
-    return render(request,'welcome.html')
+def index(request):
+    return render(request,'index.html')
 
 def home(request):
     return render(request,'../templates/home/home.html')    
